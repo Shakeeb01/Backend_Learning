7981eec03ecbf900edef00d53d0fe34c0ee3515b
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-10-05 10:32:12 +0500

    For day 3 of django learning i added the auhor model and linked it to posts model using foreign key

--- Django_Learning/posts/models.py
+++ Django_Learning/posts/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=250,unique=True)
+
 class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE,null=True,blank=True, related_name='books')
+
