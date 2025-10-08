d44f82e36e43550ad7ef7e16a8caf9f42cbe4d2d
Shakeeb ur Rehman <shakeeburrehman78@gmail.com>
2025-10-08 21:39:46 +0500

    learned about views,urls and url mapping

--- Django_Learning/posts/views.py
+++ Django_Learning/posts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
 
 # Create your views here.
+
+def home(request):
+    posts_list = Post.objects.all()
+    context = {
+        'posts':posts_list
+    }
+    return render(request,context)
+
+
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    context = {
+        'post':post
+    }
+    return render(request,context)
